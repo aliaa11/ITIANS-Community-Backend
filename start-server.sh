4e3d805b39b9f5d6921187f7634e0a6d4b4f7b5c
Aliaa <aliaaalihesh@gmail.com>
2025-07-03 19:22:37 +0300

    fix: Laravel production config for Railway

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Run Laravel optimization commands
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Start Laravel using PHP's built-in server
 php -S 0.0.0.0:${PORT:-8080} -t public
